0a825f3bc525c01b3c995365337fd3b7b0b7bea3
LowDagger <jorgeandrs2000@gmail.com>
2026-06-28 10:18:19 -0400

    changes to design

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'banco-usdt-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './service-worker.js', './icon.svg'];
+const CACHE_NAME = 'banco-usdt-compact-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
@@ -7,9 +7,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
   self.clients.claim();
 });
 
